9158f0d3b1bde3ac76a952af31c71a194d14d4d3
Marcel Mantovani <mmarcel@amdocs.com>
2018-04-05 15:33:48 -0400

    fixed logger and added count of files

--- filespace.py
+++ filespace.py
@@ -2,6 +2,7 @@
 """Script to serve as utility for tracking types of files consuming the most disk space in a given filesystem."""
 import sys
 import logging
+from path import Path
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -21,8 +22,27 @@ logger.addHandler(ch)
 
 def main():
     """ Main entry point for the script"""
+    logger.debug("Python version: {}".format(sys.version))
     logger.debug('Entering main')
+    getCountOfFiles(r'C:\Marcel\python\projects\FileSpace')
+    
     pass
 
+def getCountOfFiles(directory):
+    """ For a given path, lists all files in the directory"""
+    logger.debug('getFiles with input {}'.format(directory))
+    try:
+        d = Path(directory)
+    except IOError:
+        logger.warning('Error reading files your path' )
+    else:
+        num_files = len(d.files())
+
+    print num_files
+    pass
+
+
 if __name__ == '__main__':
-    sys.exit(main())
+#    sys.exit(main())
+    main()
+    
