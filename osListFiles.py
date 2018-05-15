47c67ac758bdddcc41017dec8489311c40bbd296
Marcel Mantovani <37183770+marcelmantovani@users.noreply.github.com>
2018-05-15 17:46:51 -0400

    added mtime, uid, gid

--- osListFiles.py
+++ osListFiles.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 
 import sys
+import time
 #from path import Path
 import os.path
 
@@ -17,23 +18,24 @@ import os.path
 
 dname = r'C:\Marcel\temp'
 
+def converTimeToStr(in_time):
+    return time.strftime("%m/%d/%Y %H:%M:%S", time.localtime(in_time))
+
 def visit (arg, dirname, names):
     
     for name in names:
         subname = os.path.join(dirname, name)
         
         if not (os.path.isdir(subname)):
-        #    print '  %s/' % name
-        #else:
-            print '%s, %s, %s ' % (os.path.dirname( subname), name, (os.path.getsize(subname)))
-            #print 'parent directory %s ' % os.path.dirname( subname)
-    #print
+            statDetail = os.stat(subname)
+            strmtime = converTimeToStr(statDetail.st_mtime)
+            print '%s, %s, %s, %s, %s, %s ' % (os.path.dirname( subname), name, (os.path.getsize(subname)) ,strmtime, statDetail.st_uid, statDetail.st_gid )
+            
     
 def main():
-    print 'dirname, filename, size'
+    print 'dirname, filename, size, mtime, uid, gid'
     os.path.walk(dname, visit, '(User data)')
 
 
 if __name__ == '__main__':
-#    sys.exit(main())
     main()
